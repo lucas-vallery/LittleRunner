fc7b9a71f934c0baf6fd07398ade0b3e717377b6
lucas-vallery <92909675+lucas-vallery@users.noreply.github.com>
2021-10-21 08:48:04 +0200

    First commit

---
+++ src/Camera.java
@@ -0,0 +1,22 @@
+public class Camera {
+    public Camera (int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    @Override
+    public String toString() {
+
+    }
+
+    private int x;
+    private int y;
+}
