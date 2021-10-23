6aaa4777da50bc1fc33823cf37a9b8b9cc45f5a4
lucas-vallery <92909675+lucas-vallery@users.noreply.github.com>
2021-10-23 12:10:37 +0200

    Displaying left and right images correctly & non-resizable window

--- src/GameScene.java
+++ src/GameScene.java
@@ -8,8 +8,10 @@ public class GameScene extends Scene {
 
     public GameScene(Group group, double v, double v1) {
         super(group, v, v1);
-        camera = new Camera(1500,0);
+        this.v = v;
+        camera = new Camera(1205,0);
         group.getChildren().add(left.getImage());
+        group.getChildren().add(right.getImage());
         render();
 
     }
@@ -17,13 +19,15 @@ public class GameScene extends Scene {
     private Camera camera;
     private StaticThings left = new StaticThings("C:\\Users\\lucas\\IdeaProjects\\runner\\img\\desert.png",800,600);
     private StaticThings right = new StaticThings("C:\\Users\\lucas\\IdeaProjects\\runner\\img\\desert.png",800,600);
+    private double v;
 
     public void render(){
         double xCam=camera.getX();
-        double offsetLeft=xCam%left.getX();
+        double offsetLeft = xCam%left.getX();
 
         left.getImage().setViewport(new Rectangle2D(offsetLeft,0,left.getX()-offsetLeft,left.getY()));
-
+        right.getImage().setViewport(new Rectangle2D(0, 0, v-(left.getX()-offsetLeft), right.getY()));
+        right.getImage().setX(left.getX()-offsetLeft);
     }
 
 }
--- src/Main.java
+++ src/Main.java
@@ -18,6 +18,7 @@ public class Main extends Application{
         Group root = new Group();
         Scene scene = new GameScene(root, 600, 400);
         primaryStage.setScene(scene);
+        primaryStage.setResizable(false);
         primaryStage.show();
     }
     public static void main(String[] args) {
